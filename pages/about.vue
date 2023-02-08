8bd44c94cf542c91102e332877148f5a1c7abf17
徐欣怡 <xuxinyi@xuxinyideiMac.local>
2023-02-09 00:48:53 +0800

    init: first commit

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>This is About page</p>
+    <nuxt-link to="/">Go to Home</nuxt-link>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>This is Home page</p>
+    <nuxt-link to="/about">Go to About</nuxt-link>
+  </div>
+</template>
